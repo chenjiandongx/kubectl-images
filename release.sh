460fdcac6d313da540176daf2d22684b0d4dd25d
chenjiandongx <chenjiandongx@qq.com>
2020-02-20 00:18:56 +0800

    Update: refactor release shell

--- release.sh
+++ release.sh
@@ -9,15 +9,16 @@ fi
 
 version=$1
 outdir=releases/$version
+bin=kubectl-images
 linux_dist=kubectl-images_linux_amd64
 darwin_dist=kubectl-images_darwin_amd64
 windows_dist=kubectl-images_windows_amd64
 
-if [ -d $outdir ]; then
+echo $outdir
+
+if [ ! -d $outdir ]; then
   mkdir -p $outdir
 fi
-
-cp LICENSE $outdir
 
 cd cmd
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ../$outdir/$linux_dist
@@ -25,9 +26,12 @@ CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o ../$outdir/$
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ../$outdir/$windows_dist
 cd ..
 
-tar cfz $outdir/$linux_dist.tar.gz -C $outdir --transform="flags=r;s|$linux_dist|kubectl-images|" $linux_dist LICENSE
-tar cfz $outdir/$darwin_dist.tar.gz -C $outdir --transform="flags=r;s|$darwin_dist|kubectl-images|" $darwin_dist LICENSE
-tar cfz $outdir/$windows_dist.tar.gz -C $outdir --transform="flags=r;s|$windows_dist|kubectl-images|" $windows_dist LICENSE
+cd $outdir
+cp $linux_dist $bin && tar cfz $linux_dist.tar.gz $bin
+cp $darwin_dist $bin && tar cfz $darwin_dist.tar.gz $bin
+cp $windows_dist $bin && tar cfz $windows_dist.tar.gz $bin
+rm $bin
+
 
 echo "Please update this file accordingly:"
 echo "https://github.com/kubernetes-sigs/krew-index/blob/master/plugins/images.yaml"
@@ -38,6 +42,6 @@ echo "VERSION: $version"
 echo
 echo "SHA256 SUMS:"
 echo "------------"
-sha256sum $outdir/$linux_dist.tar.gz
-sha256sum $outdir/$darwin_dist.tar.gz
-sha256sum $outdir/$windows_dist.tar.gz
+sha256sum $linux_dist.tar.gz
+sha256sum $darwin_dist.tar.gz
+sha256sum $windows_dist.tar.gz
